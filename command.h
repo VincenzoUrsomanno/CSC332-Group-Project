2c6dadef5f2314018579985e4e773b0b7acf20a9
Mat Garcia <leon6496@gmail.com>
2024-10-31 20:07:02 -0400

    added command abstract class

---
+++ command.h
@@ -0,0 +1,17 @@
+//
+//  command.h
+//  CSC332 Group Project
+//
+//  Created by Mathew garcia on 10/31/24.
+//
+
+#ifndef command_h
+#define command_h
+#include <string>
+
+class command{
+public:
+    virtual string execute() = 0;
+}
+
+#endif /* command_h */
