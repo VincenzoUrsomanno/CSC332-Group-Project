fedeebbb800498b52a396315ad076991e2beec82
dorelkalev <158530164+dorelkalev@users.noreply.github.com>
2024-11-21 17:05:30 -0500

    Update tree.h

--- tree.h
+++ tree.h
@@ -9,8 +9,8 @@
 class Tree : public command{ //Creates the tree class that inherits from command.h
     
 public:
-    std::string execute() override; //Execute method is overriden from command.h. This creates directories Dir0 and Dir1, with Dir1 being in Dir0.
+    std::string execute() override; //Execute method is overriden from command.h. This creates directories Dir0 and Dir1, with Dir1 being in Dir0
 private:
-    int create_directory(const char *dir_name); //Tree adds a new function to create a directory.
+    int create_directory(const char *dir_name); //Tree adds a new function to create a directory
 };
 
