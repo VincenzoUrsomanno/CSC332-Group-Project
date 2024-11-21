12c57ba5c995971af02865572f5ce481e71c2e7a
ja33289 <140358344+ja33289@users.noreply.github.com>
2024-11-21 16:22:45 -0500

    Update Path.cpp

--- Path.cpp
+++ Path.cpp
@@ -2,7 +2,7 @@
 //  Path.cpp
 //  CSC332 Group Project
 //
-//  Created by Mathew garcia on 11/8/24.
+//  Created by Arielle Uy on 11/7/24.
 //
 
 #include <stdio.h>
@@ -10,11 +10,11 @@
 #include "Path.h"
 #include "iostream"
 
-std::string Path::execute()  {
-    std::string currentPath = std::filesystem::current_path().string();
-    std::ofstream file("path.txt");
-    file << currentPath;
-    file.close();
-    std::cout << currentPath << std::endl;
-    return currentPath;  // Return path as a string
+std::string Path::execute()  { //defines Path() as an execute command 
+    std::string currentPath = std::filesystem::current_path().string(); //Retrieves the current working directory as a std::filesystem::path object and converts it to a std::string.
+    std::ofstream file("path.txt"); //Creates an output file named path.txt
+    file << currentPath; //Writes the currentPath string to the path.txt file
+    file.close(); //closes text file
+    std::cout << currentPath << std::endl; //The path is printed to the console.
+    return currentPath;  //The path is returned as a string to the caller
 }
